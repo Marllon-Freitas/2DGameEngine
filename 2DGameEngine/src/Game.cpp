9df0cefc7bcebea44a089565adc407cfc6ed0f77
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2025-01-08 16:31:48 -0300

    feat: creating a sdl window

--- 2DGameEngine/src/Game.cpp
+++ 2DGameEngine/src/Game.cpp
@@ -1,5 +1,6 @@
-#include "Game.h"
 #include <iostream>
+#include <SDL.h>
+#include "Game.h"
 
 Game::Game() {
     std::cout << "Game Contructor Called!" << std::endl;
@@ -8,7 +9,33 @@ Game::~Game() {
     std::cout << "Game Destructor Called!" << std::endl;
 }
 
-void Game::Initialize() {}
+void Game::Initialize() {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+
+    SDL_Window* window = SDL_CreateWindow(
+        "2DGameEngine",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS
+    );
+
+    if (!window) {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer) {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+        return;
+    }
+}
 
 void Game::Run() {}
 

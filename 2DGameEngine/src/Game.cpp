eaf96be73c5ee107d405a58a3f462faa1a511c92
Marllon Freitas <71530690+Marllon-Freitas@users.noreply.github.com>
2025-01-08 17:07:41 -0300

    feat: rendering a sdl window

--- 2DGameEngine/src/Game.cpp
+++ 2DGameEngine/src/Game.cpp
@@ -3,6 +3,7 @@
 #include "Game.h"
 
 Game::Game() {
+    isRuning = false;
     std::cout << "Game Contructor Called!" << std::endl;
 }
 Game::~Game() {
@@ -15,7 +16,7 @@ void Game::Initialize() {
         return;
     }
 
-    SDL_Window* window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
         "2DGameEngine",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
@@ -29,20 +30,51 @@ void Game::Initialize() {
         return;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(window, -1, 0);
 
     if (!renderer) {
         std::cerr << "Error creating SDL renderer." << std::endl;
         return;
     }
+
+    isRuning = true;
 }
 
-void Game::Run() {}
-
-void Game::ProcessInput() {}
+void Game::ProcessInput() {
+    SDL_Event sdlEvent;
+    while (SDL_PollEvent(&sdlEvent)) {
+        switch (sdlEvent.type) {
+            case SDL_QUIT:
+                isRuning = false;
+                break;
+            case SDL_KEYDOWN:
+                if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+                    isRuning = false;
+                }
+                break;
+        }
+    }
+}
 
 void Game::Update() {}
 
-void Game::Render() {}
+void Game::Render() {
+    SDL_SetRenderDrawColor(renderer, 155, 0, 0, 255);
+    SDL_RenderClear(renderer);
 
-void Game::Destroy() {}
+    SDL_RenderPresent(renderer);
+}
+
+void Game::Run() {
+    while (isRuning) {
+        ProcessInput();
+        Update();
+        Render();
+    }
+}
+
+void Game::Destroy() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
--- 2DGameEngine/src/Main.cpp
+++ 2DGameEngine/src/Main.cpp
@@ -2,5 +2,9 @@
 
 int main(int argc, char* argv[]) {
     Game game;
+
+    game.Initialize();
+    game.Run();
+    game.Destroy();
     return 0;
 }
